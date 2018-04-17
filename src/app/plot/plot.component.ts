92595ea067d185f052afdead2e67373e8d724287
WojciechKasprzyk <w.kasprzyk95@gmail.com>
2018-04-17 23:11:57 +0200

    nihil novi

--- src/app/plot/plot.component.ts
+++ src/app/plot/plot.component.ts
@@ -2,78 +2,73 @@ import { Component, OnInit, ViewChild, ElementRef, ChangeDetectionStrategy } fro
 import * as _ from 'lodash';
 @Component({
   selector: 'plot',
-  templateUrl: './plot.component.html',
+  template: `
+    <div #plot>
+
+    </div>
+  `,
   styleUrls: ['./plot.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush   // jak by się coś waliło to przez to :3 z rx'a
 })
 export class PlotComponent implements OnInit {
-  @ViewChild('plot') plot: ElementRef;
+  @ViewChild('plot') plotObject: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
-    this.funtionPlot ();
+    this.plotObject = this.plotObject.nativeElement;
+    this.funtionPlot();
   }
-
-  basicChart() {
-    const plotObject = this.plot.nativeElement;
-
-    const data = [{
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16]
-    }];
-
-    const style = {
-      margin: {t:0}
-    }
-
-    Plotly.plot( plotObject, data, style);
-  }
-
 
   funtionPlot() {
-    const plotObject = this.plot.nativeElement;
-    var frames = [
-      {name: 'sine', data: [{x: [], y: []}]},
-      {name: 'cosine', data: [{x: [], y: []}]},
-      {name: 'circle', data: [{x: [], y: []}]},
+    let frames: Frame[] = [
+      { name: 'sine', data: [{ x: [], y: [] }] },
+      { name: 'cosine', data: [{ x: [], y: [] }] },
+      { name: 'circle', data: [{ x: [], y: [] }] },
     ];
-    
-    let q = 4;
-    var n = 200*q;
-    
+
+    let q = 3;
+    var n = 200 * q;
+
     for (var i = 0; i < n; i++) {
-      var t = i / (2*n - 1) * 4 * q - 1*q;
-      
+      var t = i / (n - 1) * 2 * q
+
       // A sine wave:
       frames[0].data[0].x[i] = t * Math.PI;
       frames[0].data[0].y[i] = Math.sin(t * Math.PI);
-      
+
       // A cosine wave:
       frames[1].data[0].x[i] = t * Math.PI;
-      frames[1].data[0].y[i] = 1/2*Math.cos(2*t * Math.PI) + 1/2*Math.sin(t * Math.PI +  Math.PI/4);
-      
+      frames[1].data[0].y[i] = 1 / 2 * Math.cos(2 * t * Math.PI) + 1 / 2 * Math.sin(t * Math.PI + Math.PI / 4);
+
       // A circle:
       frames[2].data[0].x[i] = Math.sin(t * Math.PI);
       frames[2].data[0].y[i] = Math.cos(t * Math.PI);
     }
-    
-    Plotly.plot(plotObject, [{
-      x: frames[0].data[0].x,
-      y: frames[0].data[0].y,
-      line: {simplify: false},
-    }], {
-      xaxis: {range: [-q*Math.PI, q*Math.PI]},
-      yaxis: {range: [-1.2, 1.2]},
-      updatemenus: [{
-        buttons: [
-          {method: 'animate', args: [['sine']], label: 'sine'},
-          {method: 'animate', args: [['cosine']], label: 'cosine'},
-          {method: 'animate', args: [['circle']], label: 'circle'}
-        ]
-      }]
-    }, {displayModeBar: true}).then(function() {
-      Plotly.addFrames(plotObject, frames);
-    });
-  }
 
+    Plotly.plot(this.plotObject, [{
+      x0: 0,
+      dx: 1 / (n - 1) * 2 * q * Math.PI,
+      y: frames[0].data[0].y,
+      line: { simplify: false },
+    }], {
+        xaxis: { range: [-q * Math.PI, 2 * q * Math.PI] },
+        yaxis: { range: [-1.2, 1.2] },
+        updatemenus: [{
+          buttons: [
+            { method: 'animate', args: [['sine']], label: 'sine' },
+            { method: 'animate', args: [['cosine']], label: 'cosine' },
+            { method: 'animate', args: [['circle']], label: 'circle' }
+          ]
+        }]
+      }, { displayModeBar: true }).then(() => {
+        Plotly.addFrames(this.plotObject, frames);
+      });
+  }
+}
+
+
+interface Frame {
+  name: string;
+  data: Array<{ x: number[], y: number[] }>;
 }
