af5acad38081d453838a3e8a065525f153035b10
annajjj <ajonak@o2.pl>
2018-05-14 13:10:15 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,56 +8,57 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   isScrolled = false;
-  inputData =[];
+  generatedInputData=[];
+  inputVectorArray;
   isScrolledF = false;
   menuForm: FormGroup;
-  constructor(private appService: AppService, private fb: FormBuilder,) {
+  constructor(private appService: AppService, private fb: FormBuilder, ) {
     this.menuForm = this.fb.group({
       modulationType: ['BPSK', Validators.required],
       inputDataLength: [0, Validators.required],
-      inputDataType: ['random', Validators.required],
-      inputData: ['', [Validators.required, Validators.pattern('^[0-1]+$')]],
+      // inputDataType: ['random', Validators.required],
+      inputVector: ['', [Validators.required, Validators.pattern('^[0-1]+$')]],
+      inputData: [this.generatedInputData, [Validators.required, Validators.pattern('^[0-1]+$')]],
       inputFrequency: ['', Validators.required],
       bitsSpeed: ['', Validators.required]
-      // scale: ['MHz', Validators.required],
     });
-}
+  }
 
   ngOnInit(): void {
-    this.randomDataGenerator(3);
-    this.menuForm.get('inputData').valueChanges.subscribe(value => this.inputData.push(value.slice(-1)));
-    this.menuForm.get('inputDataType').valueChanges.subscribe( () => this.inputData = []);
+    this.menuForm.get('inputVector').valueChanges.subscribe(value => {
+      this.inputVectorArray = this.menuForm.get('inputVector').value.split("");
+      this.randomDataGenerator(this.menuForm.get('inputDataLength').value);
+      this.menuForm.patchValue({inputData: this.generatedInputData.toString().replace(/,/g,'')}); 
+    });
+
+    this.menuForm.get('inputDataLength').valueChanges.subscribe(value => {
+      if  (this.menuForm.get('inputVector').value!="")
+      {
+        this.randomDataGenerator(this.menuForm.get('inputDataLength').value);
+        this.menuForm.patchValue({inputData: this.generatedInputData.toString().replace(/,/g,'')}); 
+      }
+    });
   }
 
-  randomDataGenerator(number){
-    this.inputData=[];
-    let randomData=[];
-    for(let i=0; i<number;i++){
-      this.inputData.push(Math.floor(Math.random()*2))
+  randomDataGenerator(number) {
+    let tmpInputVectorArray = this.inputVectorArray.slice();
+
+    this.generatedInputData = [];
+
+    if (tmpInputVectorArray.length >= this.menuForm.get('inputDataLength').value) this.generatedInputData = tmpInputVectorArray.slice(0, this.menuForm.get('inputDataLength').value);
+    else {
+      for (let i = 0; i < number; i++) {
+        tmpInputVectorArray.unshift(tmpInputVectorArray[this.inputVectorArray.length - 1] ^ tmpInputVectorArray[tmpInputVectorArray.length - 2]);
+        this.generatedInputData.push(+tmpInputVectorArray[tmpInputVectorArray.length - 1]);
+        tmpInputVectorArray.pop();
+      }
     }
-    console.log(this.inputData);
-    for(let i=0; i<Math.pow(2, number)-1; i++){
-      this.inputData.unshift(this.inputData[this.inputData.length -1] ^ this.inputData[this.inputData.length -2]);
-      randomData.push(this.inputData[this.inputData.length -1]);
-      this.inputData.pop();
-    }
-
-    console.log(randomData);
-  }
-
-  showOptions(){
+    console.log('gen',this.generatedInputData);
   }
 
 
-  // async keyDown(key){
-  //   if(key.key != 'Backspace'){
-  //     // this.menuForm.get('inputData').valueChanges.subscribe(value => console.log(key.key))
-  //     let value = await this.menuForm.get('inputData').value +',';
-  //     await this.menuForm.patchValue({ inputData: value},{emitEvent: false})
-  //   }
-  // }
 
   @HostListener('window:scroll', ['$event'])
   onScroll(event) {
@@ -74,11 +75,9 @@ export class AppComponent implements OnInit{
     // }
   }
 
-  sliderColor(){
-    const filledPercent = this.menuForm.get('inputDataLength').value/20*100;
-    console.log(filledPercent)
-    const empty = 100 - filledPercent; 
-    console.log( "linear-gradient(to right, red" + filledPercent+"%, white" + empty +"%)");
-    return { "background": "linear-gradient(to right, red " + filledPercent+"%, white " + empty +"%)"};
-    }
+  sliderColor() {
+    const filledPercent = this.menuForm.get('inputDataLength').value / 20 * 100;
+    const empty = 100 - filledPercent;
+    return filledPercent >= 50 ? { "background": "linear-gradient(to right, #f77750 " + filledPercent + "%, #d8d4d3 " + empty + "%)" } : { "background": "linear-gradient(to left, #d8d4d3 " + empty + "%, #f77750 " + filledPercent + "%)" };
+  }
 }
