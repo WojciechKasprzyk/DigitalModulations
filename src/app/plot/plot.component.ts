61faea349a9b27591cdf593295fc80e05966382b
WojciechKasprzyk <wojciech.kasprzyk@comarch.com>
2018-05-14 16:15:58 +0200

    'balagan'

--- src/app/plot/plot.component.ts
+++ src/app/plot/plot.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, ChangeDetectionStrategy } from '@angular/core';
 import * as _ from 'lodash';
+
 @Component({
   selector: 'plot',
   template: `
@@ -8,15 +9,16 @@ import * as _ from 'lodash';
     </div>
   `,
   styleUrls: ['./plot.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush   // jak by się coś waliło to przez to :3 z rx'a
 })
 export class PlotComponent implements OnInit {
   @ViewChild('plot') plotObject: ElementRef;
   bits: number[] = [1, 0, 1, 1, 1, 0, 0, 1, 0, 1];  // @Input() lub service 
+  bitsQPSK = [2, 3, 2, 0/* powinno być 1*/, 1];
   frames: Frame[] = [
-    { name: 'sine', data: [{ x: [], y: [] }] },
-    { name: 'square', data: [{ x: [], y: [] }] },
-    { name: 'bpsk', data: [{ x: [], y: [] }] },
+    { name: 'sine', data: [{ x: [], y: [] }, { x: [], y: [] }] },
+    { name: 'square', data: [{ x: [], y: [] }, { x: [], y: [] }] },
+    { name: 'bpsk', data: [{ x: [], y: [] }, { x: [], y: [] }] },
+    { name: 'qpsk', data: [{ x: [], y: [] }, { x: [], y: [] }] },
   ];
   private scale = 2;
   private _frequency;
@@ -34,6 +36,8 @@ export class PlotComponent implements OnInit {
   get periods() {
     return this._periods;
   }
+
+
 
   constructor() { }
 
@@ -56,6 +60,8 @@ export class PlotComponent implements OnInit {
       // A sine wave:
       this.frames[0].data[0].x[i] = t * this.scale;
       this.frames[0].data[0].y[i] = Math.sin(t * this.frequency * Math.PI);
+      // this.frames[1].data[1].x[i] = t * this.scale;
+      // this.frames[1].data[1].y[i] = Math.sin(t * this.frequency * Math.PI);
     }
   }
 
@@ -64,19 +70,19 @@ export class PlotComponent implements OnInit {
   * floorowanie ucina kilka bitów
   * trzeba je uzupełnić po całym procesie
   */
-  addBit(bits) {
+  addBit(bits, where, where2, scale) {
     const timeToBit = Math.floor(this.samplingRate / bits.length);
     let index = -1;
 
     for (let i = 0, t = i;
-      i < this.samplingRate;
+      i < this.samplingRate / scale;
       i++ , t = i / ((this.samplingRate - 1) * this.frequency) * this.periods) {
-      this.frames[1].data[0].x[i] = t * this.scale;
-      if (i % timeToBit === 0) {
+      this.frames[where].data[where2].x[i] = t * this.scale;
+      if (i % (timeToBit/scale) === 0) {
         index++;
-        if(!bits[index]) bits[index]=-1; 
+        if (!bits[index]) bits[index] = -1;
       }
-      this.frames[1].data[0].y[i] = bits[index];
+      this.frames[where].data[where2].y[i] = bits[index];
     }
   }
 
@@ -84,40 +90,126 @@ export class PlotComponent implements OnInit {
     for (let i = 0, t = i;
       i < this.samplingRate;
       i++ , t = i / ((this.samplingRate - 1) * this.frequency) * this.periods) {
-        this.frames[2].data[0].x[i] = t * this.scale;
-        this.frames[2].data[0].y[i] = this.frames[0].data[0].y[i] * this.frames[1].data[0].y[i];
-      }
+      this.frames[2].data[0].x[i] = t * this.scale;
+      this.frames[2].data[0].y[i] = this.frames[0].data[0].y[i] * this.frames[1].data[0].y[i];
+    }
+  }
+
+  qpsk() {
+    this.addBit(this.bitsQPSK,3,1,2)
+    for (let i = 0, t = i;
+      i < this.samplingRate / 2;
+      i++ , t = i / ((this.samplingRate - 1) * this.frequency) * this.periods) {
+      this.frames[3].data[0].x[i] = t * this.scale;
+
+        this.frames[3].data[0].y[i] = Math.sin(t * this.frequency * Math.PI + Math.PI/4 + 2*this.frames[3].data[1].y[i] * Math.PI / 4);
+  
+    }
+  }
+
+
+  getFrame(...args: string[]) {
+    if (args.length === 0) return [{ x: [], y: [], line: { simplify: false } }];
+    let result = [];
+    args.forEach((arg: string) => {
+      const frame = this.frames.find(frame => frame.name === arg);
+      result.push({
+        x: frame.data[0].x,
+        y: frame.data[0].y,
+        name: arg,
+        line: { simplify: false }
+      })
+    });
+    return result;
   }
 
 
 
-
   funtionPlot() {
-    const bits = [1, 0, 1, 1, 1, 0, 0, 1, 0, 1]
-    this.addBit(bits);
+    this.addBit(this.bits,1,0,1);
 
     this.harmonic();
 
     this.bpsk();
 
-    Plotly.plot(this.plotObject, [{
-      x: this.frames[0].data[0].x,
-      y: this.frames[0].data[0].y,
-      line: { simplify: false },
-    }], {
-        // xaxis: { range: [0, periodsNumber * frequency * Math.PI] },
-        // yaxis: { range: [-1.2, 1.2] },
-        autosize: true,
-        title: 'wykres',
-        updatemenus: [{
-          type: 'buttons',
-          buttons: [
-            { method: 'animate', args: [['sine']], label: 'sine' },
-            { method: 'animate', args: [['square']], label: 'square' },
-            { method: 'animate', args: [['bpsk']], label: 'bpsk' },
-          ]
+    this.qpsk();
+
+    Plotly.plot(this.plotObject, this.getFrame('sine', 'square'), {
+      // xaxis: { range: [0, periodsNumber * frequency * Math.PI] },
+      // yaxis: { range: [-1.2, 1.2] },
+      autosize: true,
+      title: 'wykres',
+      updatemenus: [{
+        type: 'buttons',
+        buttons: [
+          { method: 'animate', args: [['sine']], label: 'sine' },
+          { method: 'animate', args: [['square']], label: 'square' },
+          { method: 'animate', args: [['bpsk']], label: 'bpsk' },
+          { method: 'animate', args: [['qpsk']], label: 'qpsk' },
+        ]
+      }],
+      layout: {
+        sliders: [{
+          pad: { t: 30 },
+          x: this.frames[0].data[0].x,
+          len: 0.95,
+          currentvalue: {
+            xanchor: 'right',
+            prefix: 'name: ',
+            font: {
+              color: '#888',
+              size: 20
+            }
+          },
+          transition: { duration: 500 },
+          // By default, animate commands are bound to the most recently animated frame:
+          steps: [{
+            label: 'red',
+            method: 'animate',
+            args: [['red'], {
+              mode: 'immediate',
+              frame: {redraw: false, duration: 500},
+              transition: {duration: 500}
+            }]
+          }, {
+            label: 'green',
+            method: 'animate',
+            args: [['blue'], {
+              mode: 'immediate',
+              frame: {redraw: false, duration: 500},
+              transition: {duration: 500}
+            }]
+          }, {
+            label: 'blue',
+            method: 'animate',
+            args: [['green'], {
+              mode: 'immediate',
+              frame: {redraw: false, duration: 500},
+              transition: {duration: 500}
+            }]
+          }]
         }],
-      }, { displayModeBar: true }).then(() => {
+      },
+      // updatemenus: [{
+      //   type: 'buttons',
+      //   showactive: false,
+      //   x: this.frames[0].data[0].x,
+      //   y: 0,
+      //   xanchor: 'right',
+      //   yanchor: 'top',
+      //   pad: {t: 60, r: 20},
+      //   buttons: [{
+      //     label: 'Play',
+      //     method: 'animate',
+      //     args: [null, {
+      //       fromcurrent: true,
+      //       frame: {redraw: false, duration: 1000},
+      //       transition: {duration: 500}
+      //     }]
+      //   }]
+      // }],
+  }, { displayModeBar: true })
+  .then(() => {
         Plotly.addFrames(this.plotObject, this.frames);
       });
   }
