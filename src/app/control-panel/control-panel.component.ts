012d9f60367b69e9acc7c301a8a7b90daf26571a
WojciechKasprzyk <wojciech.kasprzyk@comarch.com>
2018-05-25 16:36:14 +0200

    '.'

--- src/app/control-panel/control-panel.component.ts
+++ src/app/control-panel/control-panel.component.ts
@@ -48,7 +48,31 @@ export class ControlPanelComponent implements OnInit {
         modulationFrame.data[0].x[i] = t * this.paramsSet.scale;
         modulationFrame.data[0].y[i] = harmonicFrame.data[0].y[i] * signalFrame.data[0].y[i];
     }
-    
+  }
+
+  qpsk() { // nie testowane
+    const signalFrame = this.makeFrame('signal');
+    const modulationFrame = this.makeFrame('modulation');
+
+    for (let i = 0, t = i;
+      i < this.paramsSet.samplingRate / this.paramsSet.scale;
+      i++ , t = i / ((this.paramsSet.samplingRate - 1) * this.paramsSet.frequency) * this.paramsSet.periods) {
+      modulationFrame.data[0].x[i] = t * this.paramsSet.scale;
+
+      modulationFrame.data[0].y[i] = Math.sin(t * this.paramsSet.frequency * Math.PI + Math.PI/4 + 2 * signalFrame.data[0].y[i] * Math.PI / 4);
+    }
+  }
+
+  qam() {
+    const signalFrame = this.makeFrame('signal');
+    const modulationFrame = this.makeFrame('modulation');
+    for (let i = 0, t = i;
+      i < this.paramsSet.samplingRate / this.paramsSet.scale;
+      i++ , t = i / ((this.paramsSet.samplingRate - 1) * this.paramsSet.frequency) * this.paramsSet.periods) {
+        modulationFrame.data[0].x[i] = t * this.paramsSet.scale;
+
+
+      }
   }
 
 }
