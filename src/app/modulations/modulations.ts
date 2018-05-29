3095ca5ddfe355cdc443237e6ccb738fadbf3135
WojciechKasprzyk <wojciech.kasprzyk@comarch.com>
2018-05-29 15:49:00 +0200

    '.'

--- src/app/modulations/modulations.ts
+++ src/app/modulations/modulations.ts
@@ -1,43 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ParamsSet, Frame } from '../interfaces/interfaces';
-import { newPlotComponent } from '../new-plot/new-plot.component';
-
-@Component({
-  selector: 'control-panel',
-  template: `
-    <!--<new-plot #plot [paramsSet]="paramsSet" [signal]="signal" [modulation]="bpsk"></new-plot>-->
-  `,
-  styles: [``]
-})
-export class ControlPanelComponent implements OnInit {
-  [x: string]: any;
-  bits: number[] = [1, 0, 1, 1, 1, 0, 0, 1, 0, 1];
-  paramsSet: ParamsSet = new ParamsSet({name: 'BPSK', bits: this.bits, frequency: 50, periods: 10});
-  @ViewChild('plot') plot: newPlotComponent;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  signal(bits) {
-    const timeToBit = Math.floor(this.paramsSet.samplingRate / bits.length);
-    let index = -1;
-    const frame = this.makeFrame('signal'); // ciekawe, działa można tez przez index signature this['makeFrame']('signal');
-
-    for (let i = 0, t = i;
-      i < this.paramsSet.samplingRate / this.paramsSet.scale;
-      i++ , t = i / ((this.paramsSet.samplingRate - 1) * this.paramsSet.frequency) * this.paramsSet.periods) {
-        frame.data[0].x[i] = t * this.paramsSet.scale;
-      if (i % (timeToBit / this.paramsSet.scale) === 0) {
-        index++;
-        if (!bits[index]) bits[index] = -1;
-      }
-      frame.data[0].y[i] = bits[index];
-    }
-  }
-
-  bpsk() {
+function bpsk() {
     const harmonicFrame = this.makeFrame('harmonic');
     const signalFrame = this.makeFrame('signal');
     const modulationFrame = this.makeFrame('modulation');
@@ -51,7 +12,7 @@ export class ControlPanelComponent implements OnInit {
     }
   }
 
-  qpsk() { // nie testowane
+  function qpsk() { // nie testowane
     const signalFrame = this.makeFrame('signal');
     const modulationFrame = this.makeFrame('modulation');
 
@@ -64,7 +25,7 @@ export class ControlPanelComponent implements OnInit {
     }
   }
 
-  qam() {
+  function qam() {
     const signalFrame = this.makeFrame('signal');
     const modulationFrame = this.makeFrame('modulation');
     for (let i = 0, t = i;
@@ -75,5 +36,3 @@ export class ControlPanelComponent implements OnInit {
 
       }
   }
-
-}
